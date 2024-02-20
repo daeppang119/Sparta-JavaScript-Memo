ed7c618eba48d5c2e7c382422a45d7c5219211f1
대빵 <yuna_park@daeppang-ui-MacBookPro.local>
2024-02-20 21:35:54 +0900

    javascript 내용추가

--- week_1/01.js
+++ week_1/01.js
@@ -1,2 +1,29 @@
-// 변수, 상수
-console.log("Hello World");
+// 변수
+// 메모리에 저장한다. 읽어들여 재사용한다.
+
+// [변수의 5가지 주요 개념]
+// 변수 이름 : 저장된 값의 고유 이름
+// 변수 값 : 변수에 저장된 값
+// 변수 할당 : 변수에 값을 지정하는 범위
+// 변수 선언 : 변수를 사용하기 위해 컴퓨터에 알리는 행위
+// 변수 참조 : 변수에 할당된 값을 읽어오는 것
+
+
+// 변수를 선언할 수 있는 3가지 방법 : var, let, const
+// var
+var myVar = "Hello World"
+var myVar = "test"
+myVar = "goodbye"
+console.log(myVar)
+
+// let
+let myLet = "Hi World"
+// var myLet = "test"
+myLet = "goodbye"
+console.log(myLet)
+
+// const
+const myConst = "Hello"
+// var myConst = "test"
+// myConst = "goodbye"
+console.log(myConst)
