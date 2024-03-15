4cf64457edd72789546ba688240dcaf93d3c2a52
대빵 <yuna_park@DAEPPANG-MacBook-Pro.local>
2024-03-15 17:03:21 +0900

    일급객체로서의 함수복습

---
+++ Spart_JavaScript/week_2/07.js
@@ -0,0 +1,25 @@
+function multiplyBy(num) {
+  return function (x) {
+    return x * num;
+  };
+}
+
+function add(x, y) {
+  return x + y;
+}
+
+const multiplyByTwo = multiplyBy(2);
+// const multiplyByTwo = function (x) {
+//     return x * 2;
+// };
+
+const multiplyByThree = multiplyBy(3);
+// const multiplyByThree = function (x) {
+//     return x * 3;
+// };
+
+console.log(multiplyByTwo(10)); // 20
+console.log(multiplyByThree(10)); // 30
+
+const result = add(multiplyByTwo(5), multiplyByThree(10));
+console.log(result); // 40
